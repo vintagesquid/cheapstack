3dcc96602fd4066f6233e5cc4d54218975bdad79
Bálint Ducsai <balint.ducsai@gmail.com>
2025-08-08 00:28:27 +0200

    feat: add database seeding and resetting scripts

---
+++ src/db/reset.ts
@@ -0,0 +1,27 @@
+import { createClient } from "@libsql/client";
+import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/libsql";
+import { reset } from "drizzle-seed";
+
+import * as schema from "../server/db/schema";
+
+dotenv.config();
+
+async function main() {
+  if (!process.env.TURSO_DATABASE_URL) {
+    throw new Error("TURSO_DATABASE_URL is not defined.");
+  }
+
+  const client = createClient({
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  });
+
+  const db = drizzle(client, { casing: "snake_case" });
+
+  await reset(db, schema);
+
+  console.log("Database reset successfully.");
+}
+
+main();
