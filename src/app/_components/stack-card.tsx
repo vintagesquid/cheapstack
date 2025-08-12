6cda026bae4eae602d7749ba359d9b07e73eb3fc
Bálint Ducsai <balint.ducsai@gmail.com>
2025-08-12 12:52:47 +0200

    feat: add lines background to stack card

--- src/app/_components/stack-card.tsx
+++ src/app/_components/stack-card.tsx
@@ -1,25 +1,36 @@
-import Image from "next/image";
 import Link from "next/link";
 import type { FC } from "react";
 import type { Stack } from "~/lib/types";
+import { slugify } from "~/lib/utils";
 
 type StackCardProps = {
   stack: Stack;
 };
 
 const StackCard: FC<StackCardProps> = ({ stack }) => {
-  const slug = stack.name?.toLowerCase().replace(/\s+/g, "-");
+  const slug = slugify(stack.name || "");
+
   return (
     <Link
       href={`/stacks/${slug}`}
-      className="rounded-lg border border-gray-600 p-4 shadow"
+      className="relative rounded-lg border border-slate-600 p-6 shadow"
     >
-      <h2 className="text-2xl">{stack.name}</h2>
+      <div
+        className="absolute inset-2"
+        style={{
+          backgroundImage: "url('/images/diagonal-lines.svg')",
+          backgroundSize: "24px",
+          opacity: 0.165,
+        }}
+      ></div>
 
-      <div className="flex gap-2 break-words">
-        {stack.technologies.map((tech) => (
-          <div key={tech.id}>{tech.provider?.slice(0, 15)}</div>
-        ))}
+      <div className="space-y-2">
+        <h2 className="text-2xl">{stack.name}</h2>
+        <div className="flex flex-wrap gap-2 break-words">
+          {stack.technologies.map((tech) => (
+            <div key={tech.id}>{tech.provider?.slice(0, 15)}</div>
+          ))}
+        </div>
       </div>
     </Link>
   );
