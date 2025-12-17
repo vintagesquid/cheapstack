b20ca10f98a2e91d739c5cd0a71b988f24c8de6a
Bálint Ducsai <balint.ducsai@gmail.com>
2025-12-17 15:05:41 +0100

    fix(env): remove DATABASE_URL from server and runtime environment variables

--- src/env.js
+++ src/env.js
@@ -7,7 +7,6 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -30,7 +29,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
 
     TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
